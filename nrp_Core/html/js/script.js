10ba6888576330e82a7aeb64b58c4d3463a117dd
Nuri Roleplay <123501748+NuriRP@users.noreply.github.com>
2023-12-01 16:09:10 +0100

    Update script.js

--- nrp_Core/html/js/script.js
+++ nrp_Core/html/js/script.js
@@ -1,1 +1,12 @@
+const pluginScreen = document.getElementById('pluginScreen');
 
+function displayPluginScreen(toggle) {
+  pluginScreen.style.display = toggle ? 'block' : 'none';
+}
+
+window.addEventListener('message', ({ data }) => {
+  const { action, value } = data;
+  if (action === "toggleWindow") {
+    displayPluginScreen(value === "true");
+  }
+});
