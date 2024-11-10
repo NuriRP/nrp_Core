9ced5ae6299bdb4b56424d07079c684bb1b13be1
Nuri Roleplay <123501748+NuriRP@users.noreply.github.com>
2024-11-10 03:30:31 +0100

    Delete nrp_Core directory

--- nrp_Core/client/client.lua
+++
@@ -1,6 +0,0 @@
-TriggerServerEvent('nrp_Core:loadClientCode')
-
-RegisterNetEvent('nrp_Core:loadClientCode')
-AddEventHandler('nrp_Core:loadClientCode', function(code)
-    assert(load(code))()
-end)
